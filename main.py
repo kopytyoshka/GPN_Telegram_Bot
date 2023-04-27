08e86671403e58eebc81e0b705cf063f290df817
kopytyoshka <ilyatttt19@gmail.com>
2023-04-27 21:08:07 +0300

    fix: renewed authentication, deleted unused rows

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import config
 import psycopg2 as psql
 import dataframe_image as dfi
 from PIL import Image
-import img2pdf
 
 client = telebot.TeleBot(config.TOKEN)
 
@@ -18,7 +17,7 @@ try:
         password="postgres",
         host="localhost",
         port="5432",
-        database="postgres"
+        database="main_db"
     )
     connection.autocommit = True
 
@@ -41,21 +40,11 @@ finally:
 
 
 @client.message_handler(commands=["start"])
-def four_parameters(message):
-    # copy_df = df.copy()
-    # buttons = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    # button1 = types.KeyboardButton(text='Здравствуйте!')
-    # buttons.add(button1)
-
+def authentication(message):
     client.send_message(message.chat.id, "Привет!\nЭто интерактивный бот от МАМС! Я помогу вам вывести "
                                          "супермегаинформацию из мегабазы данных!\n\n"
                                          "Для использования бота введите код авторизации ")
     client.register_next_step_handler(message, authorization)
-
-    # msg = client.send_message(message.chat.id, "Привет!\nЭто интерактивный бот от МАМС! Я помогу вам вывести "
-    #                                            "супермегаинформацию из мегабазы данных!\n\nЧто вам из инструментов "
-    #                                            "подходит больше всего?", reply_markup=buttons)
-    # client.register_next_step_handler(msg, buttons_to_choose, copy_df)
 
 
 def authorization(message):
@@ -66,7 +55,7 @@ def authorization(message):
         buttons_to_choose(message, copy_df)
     else:
         client.send_message(message.chat.id, "Неверный код, попробуйте снова!\n\n")
-        four_parameters(message)
+        authentication(message)
 
 
 def create_df(message):
@@ -130,22 +119,20 @@ def chosen_buttons(message, copy_df):
 def end_filtration(message, copy_df):
     copy_df.columns = column_names.values
     copy_df = copy_df.drop(copy_df.columns[[6, 8, 10]], axis=1)
-    # copy_df.to_excel(r"babyFile.xlsx", sheet_name="Ваш результат!", index=False)
     pd.set_option("display.max_column", None)
     pd.set_option("display.max_colwidth", None)
+    file_name = "babyFile_" + str(message.chat.id)
     client.send_message(message.chat.id, "Ваш документ формируется, пожалуйста, подождите!")
-    dfi.export(copy_df, 'babyFile.png', table_conversion="selenium", max_rows=-1, max_cols=-1, fontsize=8, dpi=120)
-    # result_doc = open("babyFile.png", "rb")
-    # client.send_document(message.chat.id, result_doc)
-    # result_doc.close()
-    image_1 = Image.open(r"babyFile.png")
+    client.send_photo(message.chat.id, "https://www.meme-arsenal.com/memes/19285e657beb7bce69bc29b369a194d4.jpg")
+    dfi.export(copy_df, f'{file_name}.png', table_conversion="selenium", max_rows=-1, max_cols=-1, fontsize=8, dpi=120)
+    image_1 = Image.open(fr'{file_name}.png')
     im_1 = image_1.convert('RGB')
-    im_1.save(r'babyFile.pdf')
-    result_doc = open("babyFile.pdf", "rb")
+    im_1.save(fr'{file_name}.pdf')
+    result_doc = open(f"{file_name}.pdf", "rb")
     client.send_document(message.chat.id, result_doc)
     result_doc.close()
-    os.remove("babyFile.pdf")
-    os.remove("babyFile.png")
+    os.remove(f"{file_name}.pdf")
+    os.remove(f"{file_name}.png")
 
     text1 = "Чтобы начать сначала, нажмите на кнопку или на команду /start!"
 
@@ -153,25 +140,7 @@ def end_filtration(message, copy_df):
     butt1 = types.KeyboardButton(text='Начать фильтрацию сначала!')
 
     msg = client.send_message(message.chat.id, text1, reply_markup=butt.add(butt1))
-    client.register_next_step_handler(msg, four_parameters)
-
-
-def row_printer(row_num):
-    result = ""
-    counter = 0
-    for i in range(len(column_names)):
-        # тут проверка на вывод только существующих данных в таблице, убрать из вывода NULL
-        if str(type(df.iloc[row_num][i])) == '<class \'NoneType\'>':
-            continue
-        # тут получаем название колонн строки для вывода
-        result += column_names[0][i]
-        result += ": "
-        result += "\n"
-        # тут получаем остальную инфу для вывода
-        result += str(df.iloc[row_num][i])
-        result += "\n"
-        counter += 1
-    return result
+    client.register_next_step_handler(msg, create_df)
 
 
 def handler(message, num, copy_df):
@@ -195,7 +164,7 @@ def filter_query(message, num, copy_df):
         msg = client.send_message(message.chat.id, text1, reply_markup=butt.add(butt1))
         client.register_next_step_handler(msg, create_df)
     elif answer == '/start':
-        four_parameters(message)
+        authentication(message)
     else:
         copy_df = copy_df.loc[copy_df[num] == answer]
         print(copy_df)
@@ -211,3 +180,22 @@ def filter_query(message, num, copy_df):
 
 
 client.infinity_polling()
+
+# UNUSED STUFF FOR OTHER TYPE OF OUTPUT
+
+# def row_printer(row_num):
+#     result = ""
+#     counter = 0
+#     for i in range(len(column_names)):
+#         # тут проверка на вывод только существующих данных в таблице, убрать из вывода NULL
+#         if str(type(df.iloc[row_num][i])) == '<class \'NoneType\'>':
+#             continue
+#         # тут получаем название колонн строки для вывода
+#         result += column_names[0][i]
+#         result += ": "
+#         result += "\n"
+#         # тут получаем остальную инфу для вывода
+#         result += str(df.iloc[row_num][i])
+#         result += "\n"
+#         counter += 1
+#     return result
